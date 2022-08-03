4d7bc6bac1afa4412aa25c859d54ec4e73fbc7b5
slipgrass <histo-ann@yandex.ru>
2022-08-04 01:12:59 +0500

    Добавлены формулировки задач и строки документации в задаче 3.7

--- HT3 Task 3.7!.py
+++ HT3 Task 3.7!.py
@@ -1,18 +1,30 @@
+#Продолжить работу над заданием. В программу должна попадать строка из слов, разделённых
+#пробелом. Каждое слово состоит из латинских букв в нижнем регистре. Нужно сделать вывод
+#исходной строки, но каждое слово должно начинаться с заглавной буквы. Используйте
+#написанную ранее функцию int_func().
+
 def int_func():
-    list_words = input("Введите несколько слов строчными латинскими буквами: ").lower().split()
-    for word in list_words:
+    for word in phrase:
         items = 0
         for item in word:
             if ord(item) >= 97 and ord(item) <= 122:
+                """Проверяет принадлежнось каждого символа к латинским"""
                 items += 1
+                """Прибaваляет проверенный элемент к остальным, если цикл прошёл"""
 
-        print(word.title() if items == len(word) else f'{list_words} only small latin letters')
+                print(phrase.title() if items == len(phrase) else f'{phrase} only small latin letters')
+                """Если все слова прошли цикл успешно, то печатает их с заглавной,
+        
+                если нет - прекращает работу и выдаёт сообщение
+        
+                """
 
-phrase = input("Введите фразу строчными латинскими буквами: ").split()
+phrase = input("Введите фразу строчными латинскими буквами: ")
 for i in phrase:
-    result = int_func(i)
-    if result:
-        print(result, ' ')
+    result = int_func()
+    #if result:
+        #print(result, ' ')
+
 
 
 
